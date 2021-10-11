2215ae3d33bd38374bb607616bfcff52e145e8ce
unknown <parul3kin@gmail.com>
2021-10-12 00:15:06 +0530

    First Commit

--- OR.py
+++ OR.py
@@ -6,30 +6,36 @@ from utilities.Perceptron_class import Perceptron
 from utilities.all_ImpFunctions import prepare_data, save_model, save_plot
 
 # DATASET
-OR = {
-    'x1' : [0, 0, 1, 1],
-    'x2' : [0, 1, 0, 1],
-    'y' : [0, 1, 1, 1]
-}
-df1 = pd.DataFrame(OR)
-print(df1)
+def main(data, eta, epochs, modelFilename, plotFilename):
+    
+    df = pd.DataFrame(data)
+    print(df)
 
-# PREPARE DATA
-x, y = prepare_data(df1)
+    # PREPARE DATA
+    x, y = prepare_data(df)
 
-# FIT MODEL
-ETA = 0.3 # SHOULD BE BETWEEN 0 AND 1
-EPOCHS = 10
+    # FIT MODEL
+    model = Perceptron(ETA, EPOCHS)
+    model.fit(x, y)
 
-model_OR = Perceptron(ETA, EPOCHS)
-model_OR.fit(x, y)
+    # PREDICTIONS AND TOTAL LOSS
+    model.predict(x)
+    model.total_loss()
 
-# PREDICTIONS AND TOTAL LOSS
-model_OR.predict(x)
-model_OR.total_loss()
+    # SAVE MODEL
+    save_model(model, modelFilename)
 
-# SAVE MODEL
-save_model(model_OR, 'OR.model')
+    # SAVE PLOT
+    save_plot(df, plotFilename, model)
 
-# SAVE PLOT
-save_plot(df1, "OR.png", model_OR)
+if __name__ == '__main__': # entry point from where the cide will start executing
+    OR = {
+        'x1' : [0, 0, 1, 1],
+        'x2' : [0, 1, 0, 1],
+        'y' : [0, 1, 1, 1]
+    }
+
+    ETA = 0.3 # SHOULD BE BETWEEN 0 AND 1
+    EPOCHS = 10
+
+    main(data = OR, eta = ETA, epochs = EPOCHS, modelFilename = 'OR.model', plotFilename = 'OR.png')
--- XOR.py
+++ XOR.py
@@ -6,30 +6,37 @@ from utilities.Perceptron_class import Perceptron
 from utilities.all_ImpFunctions import prepare_data, save_model, save_plot
 
 # DATASET
-XOR = {
+
+def main(data, eta, epochs, modelFilename, plotFilename):
+    
+    df = pd.DataFrame(data)
+    print(df)
+
+    # PREPARE DATA
+    x, y = prepare_data(df)
+
+    # FIT MODEL
+    model = Perceptron(ETA, EPOCHS)
+    model.fit(x, y)
+
+    # PREDICTIONS AND TOTAL LOSS
+    model.predict(x)
+    model.total_loss()
+
+    # SAVE MODEL
+    save_model(model, modelFilename)
+
+    # SAVE PLOT
+    save_plot(df, plotFilename, model)
+
+if __name__ == '__main__': # entry point from where the cide will start executing
+    XOR = {
     'x1' : [0, 0, 1, 1],
     'x2' : [0, 1, 0, 1],
     'y' : [0, 1, 1, 0]
 }
-df2 = pd.DataFrame(XOR)
-print(df2)
 
-# PREPARE DATA
-x, y = prepare_data(df2)
+    ETA = 0.3 # SHOULD BE BETWEEN 0 AND 1
+    EPOCHS = 10
 
-# FIT MODEL
-ETA = 0.3 # SHOULD BE BETWEEN 0 AND 1
-EPOCHS = 10
-
-model_XOR = Perceptron(ETA, EPOCHS)
-model_XOR.fit(x, y)
-
-# PREDICTIONS AND TOTAL LOSS
-model_XOR.predict(x)
-model_XOR.total_loss()
-
-# SAVE MODEL
-save_model(model_XOR, 'XOR.model')
-
-# SAVE PLOT
-save_plot(df2, "XOR.png", model_XOR)
+    main(data = XOR, eta = ETA, epochs = EPOCHS, modelFilename = 'XOR.model', plotFilename = 'XOR.png')
